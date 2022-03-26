8686006b52b8b9de27f583a65396dea839142760
meowsome <tycarr01@gmail.com>
2022-03-26 18:32:53 -0400

    Temporarily disable schedule for scraping images

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 from scrape.scrape_images import scrape_images
 from posting.post_image import post_image
 
-schedule.every(3).weeks.do(scrape_images)
+# schedule.every(3).weeks.do(scrape_images)
 schedule.every().day.at("13:00").do(post_image)
 
 while True:
