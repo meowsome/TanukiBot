8e83fb06cf706b28505ec4c60ce74736044e321a
meowsome <tycarr01@gmail.com>
2022-03-26 17:57:32 -0400

    Update server.py to reference functions that kick off others Set while loop delay to 60 sec

--- server.py
+++ server.py
@@ -1,25 +1,11 @@
-import tweepy
 import schedule
 import time
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from scrape.scrape_images import scrape_images
+from posting.post_image import post_image
 
-auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET")) 
-auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
-api = tweepy.API(auth)
-
-user = api.verify_credentials()
-
-def post_image():
-    image = find_image()
-    print(image)
-
-def find_image():
-    return "test"
-
-schedule.every().second.do(post_image)
+schedule.every(3).weeks.do(scrape_images)
+schedule.every().day.at("13:00").do(post_image)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(60) # sleep for this many seconds
