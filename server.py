6c4d2ea7ab652ba87b71868bd28f4583e22f61f8
meowsome <tycarr01@gmail.com>
2022-03-27 00:12:46 -0400

    Create function for creating directory and run before jobs start

--- server.py
+++ server.py
@@ -7,20 +7,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-directory = "tmp"
-Path(directory).mkdir(parents=True, exist_ok=True)
-Path(f"{directory}/bad").mkdir(parents=True, exist_ok=True)
-Path(f"{directory}/thumbs").mkdir(parents=True, exist_ok=True)
-Path(f"{directory}/pending_post").mkdir(parents=True, exist_ok=True)
+def create_directory():
+    directory = "tmp"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    Path(f"{directory}/bad").mkdir(parents=True, exist_ok=True)
+    Path(f"{directory}/thumbs").mkdir(parents=True, exist_ok=True)
+    Path(f"{directory}/pending_post").mkdir(parents=True, exist_ok=True)
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=os.getenv("POSTING_TIME_UTC"))
 def post_image_schedule():
+    create_directory()
     post_image()
 
 @sched.scheduled_job('cron', day=1)
 def scrape_images_schedule():
+    create_directory()
     scrape_images()
 
 try:
